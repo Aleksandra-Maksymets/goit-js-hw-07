33dba995abf6715f79d1117b32764c9682b62f8f
Aleksandra Maksymets <omelchenko.aleksandra@gmail.com>
2021-10-25 21:59:06 +0100

    adds basiclightbox library and gallary Markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+galleryContainerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
