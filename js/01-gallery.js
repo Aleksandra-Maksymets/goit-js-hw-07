087dc5026adcfd5e55e68b26da865b6cb153ed10
Aleksandra Maksymets <omelchenko.aleksandra@gmail.com>
2021-10-26 00:48:37 +0100

    adds completed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Gains reference of the container
 const galleryContainerRef = document.querySelector(".gallery");
-const galleryMarkup = createGalleryItems(galleryItems);
-galleryContainerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
 
+// Renders gallery cards
 function createGalleryItems(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -22,3 +21,46 @@ function createGalleryItems(galleryItems) {
     })
     .join("");
 }
+
+// Adds created markup to the 01-gallery.html
+const galleryMarkup = createGalleryItems(galleryItems);
+galleryContainerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+// Adds eventListener to the gallaryContainer using delegation
+galleryContainerRef.addEventListener("click", onImageClick);
+
+// Functions
+
+// Click handler
+function onImageClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let bigImageLink = evt.target.dataset.source;
+  createModalWindow(bigImageLink);
+
+  onEscapeCloseModal();
+}
+
+// Creates modal window using library
+function createModalWindow(link) {
+  const instance = basicLightbox.create(`
+    <img src="${link}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+// Closes modal window using Escape button from keyboard
+function onEscapeCloseModal() {
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+// console.log(galleryItems);
